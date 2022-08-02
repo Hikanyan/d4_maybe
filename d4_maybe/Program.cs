a56b14ca1d6656d29001791302d23a07573259d0
Hikanyan <hikanyan0131@gmail.com>
2022-08-02 21:36:36 +0900

    01

--- d4_maybe/Program.cs
+++ d4_maybe/Program.cs
@@ -1,1 +1,11 @@
 ﻿//https://paiza.jp/works/mondai/drankfast/d4_maybe
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var line = Console.ReadLine().Split(',');
+        Console.WriteLine($"{line[0]}!!");
+    }
+}
